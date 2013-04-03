9557037ffb4cfe8a4332d1d245b681464bb79b85
Peter Rust <peter@cornerstonenw.com>
2013-04-03 10:22:39 -0700

    Split out template loading

--- glass_templates.js
+++ glass_templates.js
@@ -2,82 +2,48 @@
 
 var is_node = typeof require != 'undefined';
 if (is_node)
-  module.exports = template;
+  module.exports = GlassTemplates;
 else
-  root.template = template;
+  root.GlassTemplates = GlassTemplates;
 
-var cache = {};
-var regex = /\{\{(\{)?(.+?)\}\}\}?/g;
-var html_regex = /\{\{(\{)?(.+?)\.html\}\}\}?/gi;
+var regex = /\{\{(\{)?\s*(.+?)\s*\}\}\}?/g;
+var nested_regex = /\{\{(\{)?\s*(.+?)\.html\s*\}\}\}?/gi;
 
-function template(path, obj, callback) {
-  preload(path, function(err) {
-    if (err) return callback(err);
-    _template(cache[path], obj, callback);
-  });
-}
+function GlassTemplates(templates) {
+  if (typeof templates == 'string')
+    templates = {'default': templates};
 
-function templateSync(path, obj) {
-  return _template(cache[path], obj);
-}
+  function template(obj, tmpl_name) {
+    var template = templates[tmpl_name || 'default'];
+    if (!template)
+      throw new Error('Template "' + (tmpl_name || 'default') + '" not found.');
 
-function _template(template, obj, callback) {
-  var result = template.replace(regex, function(match, third_brace, key) {
-    if (key.slice(-5).toLowerCase() == '.html') {
-      if (!(key in cache))
-        throw new Error('template "' + key + '" not preloaded');
-      return _template(cache[key], obj[key.slice(0, -5)] || obj);
-    }
+    return template.replace(regex, function(match, third_brace, key) {
+      if (nested_regex.test(match)) {
+        var separator_ix = key.indexOf(' ');
+        if (separator_ix > -1) {
+          var nested_obj = obj[key.slice(0, separator_ix)];
+          key = key.slice(separator_ix + 1);
+        }
 
-    var val = obj[key];
-    if (val == null)
-      return '';
+        if (!(key in templates))
+          throw new Error('template "' + key + '" not preloaded');
+        return template(templates[key], nested_obj || obj);
+      }
 
-    return third_brace ? val : escape(val);
-  });
-  
-  if (callback)
-    callback(null, result);
-  else
-    return result;
-}
+      var val = obj[key];
+      if (val == null)
+        return '';
 
-// TODO: pull out the template-preloader into a separate npm module
-// so glass-templates can be passed the templates
-function preload(path, callback) {
-  function cb(err, template) {
-    if (err) return callback(err);
-
-    cache[path] = template;
-
-    var nested_templates = template.match(html_regex);
-    if (!nested_templates.length)
-      return callback();
-
-    // TODO: inline _.after() to remove the underscore dependency
-    var cb = _.after(nested_templates.length, callback);
-    nested_templates.forEach(function(path) {
-      preload(path, cb);
+      return third_brace ? val : escape(val);
     });
   }
 
-  if (is_node)
-    require('fs').readFile(path, 'utf-8', cb);
-  else if (root.$)
-    $.ajax(path, ajaxSettings(cb));
-  else
-    throw new Error('When running in the browser, jQuery/Zepto is necessary to pull in templates via ajax');
+  return {'template': template};
 }
 
-function ajaxSettings(callback) {
-  return {
-    'success': function(data) {
-      callback(null, data);
-    },
-    'error': function(jqXHR, textStatus, errorThrown) {
-      callback(errorThrown);
-    }
-  }
+GlassTemplates.template = function(templ, obj) {
+  return GlassTemplates(templ).template(obj);
 }
 
 // from _.template()
